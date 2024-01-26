2720c3aaf209df6235c66f65c8b390c54e29b3d9
jrbench0925 <ben07010@byui.edu>
2024-01-25 18:05:02 -0700

    hamburger

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -2,3 +2,11 @@ document.getElementById("copyright").innerHTML = new Date().getFullYear()
 
 var modifiedDate = document.lastModified
 document.getElementById("lastModified").innerHTML = modifiedDate;
+
+const hamburgerButton = document.querySelector("#hamburger");
+const mainNav = document.querySelector(".navigation");
+
+hamburgerButton.addEventListener('click', () => {
+    mainNav.classList.toggle('show');
+    hamburgerButton.classList.toggle('show');
+});
