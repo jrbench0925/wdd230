d6d4e133dcbb9b162569f0ddc119a9a3d6a5f861
jrbench0925 <ben07010@byui.edu>
2024-02-10 21:22:49 -0700

    fixed modes

--- chamber/scripts/dark.js
+++ chamber/scripts/dark.js
@@ -1,12 +1,14 @@
 
 const modeSwitch = document.getElementById("modeSwitch");
- 
-modeSwitch.addEventListener("change", function() {
+
+modeSwitch.addEventListener("change", function () {
     const body = document.body;
- 
+
     if (modeSwitch.checked) {
         body.classList.add("dark-mode");
+        body.classList.remove("light-mode");
     } else {
         body.classList.remove("dark-mode");
+        body.classList.add("light-mode");
     }
 });
