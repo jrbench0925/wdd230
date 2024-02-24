b39193bd1f54b6872bbd3d2449a51ceeb3d909be
jrbench0925 <ben07010@byui.edu>
2024-02-24 15:05:47 -0700

    visit

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -13,15 +13,4 @@ hamburgerButton.addEventListener('click', () => {
     hamburgerButton.classList.toggle('show');
 });
 
-const visitsDisplay = document.querySelector(".visits");
 
-let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
-
-if (numVisits !== 0) {
-    visitsDisplay.textContent = numVisits;
-} else {
-    visitsDisplay.textContent = "This is your first visit. Welcome!";
-}
-
-numVisits++;
-localStorage.setItem("numVisits-ls", numVisits);
