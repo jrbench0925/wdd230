0f59eedd44b2fa60a1b9f7763cbf75f1e26463fd
jrbench0925 <ben07010@byui.edu>
2024-03-23 22:04:22 -0600

    removed junk

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -4,7 +4,6 @@ const display = document.querySelector("#membersData");
 const url = "https://jrbench0925.github.io/wdd230/chamber/data/members.json";
 const membersData = document.querySelector('#membersData');
 
-
 gridbutton.addEventListener("click", () => {
 	display.classList.add("grid");
 	display.classList.remove("list");
@@ -16,8 +15,7 @@ function showList() {
 	display.classList.add("list");
 	display.classList.remove("grid");
 }
-
-let mode = 'grid'; 
+let mode = 'grid';
 
 async function getLinks() {
     const response = await fetch(url);
@@ -64,71 +62,3 @@ listbutton.addEventListener("click", () => {
     mode = 'list';
     getLinks();
 });
-
-/*const gridbutton = document.querySelector("#grid");
-const listbutton = document.querySelector("#list");
-const display = document.querySelector("membersData");
-const url = "https://jrbench0925.github.io/wdd230/chamber/data/members.json";
-const membersData = document.querySelector('#membersData');
-
-gridbutton.addEventListener("click", () => {
-    display.classList.add("grid");
-    display.classList.remove("list");
-});
-
-listbutton.addEventListener("click", showList);
-
-function showList() {
-    display.classList.add("list");
-    display.classList.remove("grid");
-}
-
-
-let mode = 'grid';
-
-async function getLinks() {
-    const response = await fetch(url);
-    const data = await response.json();
-    displayLinks(data.members);
-}
-
-function displayLinks(links) {
-    membersData.innerHTML = '';
-    links.forEach(link => {
-        const article = document.createElement('article');
-        article.classList.add('directorylist');
-
-
-        if (mode === 'grid') {
-            article.innerHTML = `
-                <img src="${link.logo}" alt="${link.name}">
-                <p>${link.address}</p>
-                <p>${link.phone}</p>
-                <p><a href="${link.website}">${link.website}</a></p>
-                <p>${link.membershipLevel}</p>
-            `;
-        } else if (mode === 'list') {
-            article.innerHTML = `
-                <h3>${link.name}</h3>
-                <p>${link.address}</p>
-                <p>${link.phone}</p>
-                <p><a href="${link.website}">${link.website}</a></p>
-                <p>${link.membershipLevel}</p>
-            `;
-        }
-
-        membersData.appendChild(article);
-    });
-}
-
-getLinks();
-
-gridbutton.addEventListener("click", () => {
-    mode = 'grid';
-    getLinks();
-});
-
-listbutton.addEventListener("click", () => {
-    mode = 'list';
-    getLinks();
-}); */
