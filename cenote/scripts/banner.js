a08e7c3364776904c267a95bf786ffc0d00a52c2
jrbench0925 <ben07010@byui.edu>
2024-04-06 09:59:55 -0600

    removed unused code

--- cenote/scripts/banner.js
+++ cenote/scripts/banner.js
@@ -1,14 +1,3 @@
-/* Removed Call To action button
-document.addEventListener('DOMContentLoaded', function () {
-    const closeButton = document.getElementById('closeButton');
-    const reservationBanner = document.getElementById('reservation-banner');
-
-    closeButton.addEventListener('click', function () {
-        reservationBanner.style.display = 'none';
-    });
-});
-*/
-
 document.addEventListener('click', function (event) {
     if (event.target && event.target.id === 'tempButton') {
         const highTempMessage = document.getElementById('high-temp-message');
