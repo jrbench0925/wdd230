df24433e9c0d18757b9a73b20b7eaeff1281d698
jrbench0925 <ben07010@byui.edu>
2024-04-10 18:29:58 -0600

    thank you pages

--- cenote/scripts/get-date.js
+++ cenote/scripts/get-date.js
@@ -1,9 +1,11 @@
-document.getElementById("copyright").innerHTML = new Date().getFullYear()
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("copyright").innerHTML = new Date().getFullYear();
 
-const hamburgerButton = document.querySelector("#hamburger");
-const mainNav = document.querySelector(".navigation");
+    const hamburgerButton = document.querySelector("#hamburger");
+    const mainNav = document.querySelector(".nav-links");
 
-hamburgerButton.addEventListener('click', () => {
-    mainNav.classList.toggle('show');
-    hamburgerButton.classList.toggle('show');
+    hamburgerButton.addEventListener('click', () => {
+        mainNav.classList.toggle('show');
+        hamburgerButton.classList.toggle('show');
+    });
 });
